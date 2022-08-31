64b8b568d3515635832a69b0adae6e129e35fb43
kimjaebeom1 <gojn0214@gmail.com>
2022-08-31 10:40:20 +0900

    :rocket: Deploy firstSettings(#1)
    프론트엔드 배포를 위한 초기세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
+    "plugin:react/recommended",
+    "standard-with-typescript",
     "standard",
-    "prettier"
+    "prettier",
   ],
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
-  }
-}
+    "react/react-in-jsx-scope": "off",
+    "react/display-name": "off",
+  },
+};
