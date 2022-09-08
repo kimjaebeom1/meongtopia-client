878ac03244a4e6e6ea63a782af72c62a529f30c8
cfgop23 <103302206+cfgop23@users.noreply.github.com>
2022-09-08 22:28:43 +0900

    :art: Update header & mypage update
    헤더 마이페이지 수정

--- src/components/commons/layout/sidebar/Sidebar.presenter.tsx
+++ src/components/commons/layout/sidebar/Sidebar.presenter.tsx
@@ -1,5 +1,8 @@
+import { useQuery } from "@apollo/client";
 import MenuButton from "../../buttons/menu";
+import { FETCH_USER } from "./Sidebar.queries";
 import * as Sidebar from "./Sidebar.styles";
+import { IMyPageSidebarUIProps } from "./Sidebar.types";
 
 const USER_MENUS = [
   { page: "/mypage/user/reserve", title: "예약 내역" },
@@ -7,21 +10,40 @@ const USER_MENUS = [
   { page: "/mypage/user/review", title: "내가 쓴 리뷰" },
 ];
 
-export default function MyPageSidebarUI() {
+export default function MyPageSidebarUI(props: IMyPageSidebarUIProps) {
+  const { data } = useQuery(FETCH_USER);
+  const userInfo = data?.fetchUser;
+
   return (
     <Sidebar.Wrapper>
       <Sidebar.InfoContainer>
         <Sidebar.List>
           <Sidebar.profileImg />
         </Sidebar.List>
-        <Sidebar.List>님 안녕하세요!</Sidebar.List>
         <Sidebar.List>
-          <span>포인트</span>
-          <Sidebar.Cost>0</Sidebar.Cost>
-          <span>원</span>
+          <Sidebar.InfoText>{userInfo?.nickname}</Sidebar.InfoText>
+          <span>님 안녕하세요!</span>
         </Sidebar.List>
-        <Sidebar.List>회원정보 수정</Sidebar.List>
-        <Sidebar.List>로그아웃</Sidebar.List>
+        <Sidebar.List>
+          <Sidebar.PointIcon />
+          <span>내 포인트:&nbsp;</span>
+          <Sidebar.InfoText>
+            {userInfo?.point.toLocaleString()}
+          </Sidebar.InfoText>
+          <span>원</span>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <Sidebar.Charge>충전하기</Sidebar.Charge>
+        </Sidebar.List>
+        <Sidebar.List>
+          <Sidebar.UpdateIcon />
+          <Sidebar.ClickSpan>회원정보 수정</Sidebar.ClickSpan>
+        </Sidebar.List>
+        <Sidebar.List>
+          <Sidebar.LogoutIcon />
+          <Sidebar.ClickSpan onClick={props.onCLickLogout}>
+            로그아웃
+          </Sidebar.ClickSpan>
+        </Sidebar.List>
       </Sidebar.InfoContainer>
       <Sidebar.MenuContainer>
         {USER_MENUS.map((el) => (
