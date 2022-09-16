c1e23f0062afde1cb03a205d67f36e4dffa0af89
wonny2 <onehee2choi@gmail.com>
2022-09-16 14:17:47 +0900

    :sparkles: [Update] laning page
    랜딩..수정수정수정

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -21,7 +21,7 @@ export default function Layout(props: ILayoutProps) {
     <LayoutWrapper>
       {/* <LayoutHeader /> */}
       <LayoutBody>{props.children}</LayoutBody>
-      {/* <LayoutFooter /> */}
+      <LayoutFooter />
     </LayoutWrapper>
   );
 }
