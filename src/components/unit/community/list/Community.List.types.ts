bba79ff89172d51a3113b461288c4d2628371acb
cfgop23 <103302206+cfgop23@users.noreply.github.com>
2022-09-20 21:17:24 +0900

    yarn build

--- src/components/unit/community/list/Community.List.types.ts
+++
@@ -1,7 +0,0 @@
-// import { IQuery } from "../../../../commons/types/generated/types";
-// import { MouseEvent } from "react";
-
-// // export interface ICommunityListUIProps {
-// //   data?: Pick<IQuery, "fetchBoards">;
-// //   onClickMoveToDetail: (event: MouseEvent<HTMLDivElement>) => void;
-// // }
